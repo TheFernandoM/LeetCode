63ad3c2b602e2504a37c2a9c0ca0570e662ee0d6
Fernando Molina <famr_77@hotmail.com>
2022-06-13 22:44:30 -0400

    Added Problem #3, solution and tests

---
+++ Solutions/C#/Problem3_LongestSubstring/Problem3LengthOfLongestSubstring.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Solutions.Cs.Problem3LengthOfLongestSubstring
+{
+    /// <summary>
+    /// #3: Longest Substring Without Repeating Characters
+    /// 
+    /// Given a string s, find the length of the longest substring without repeating characters.
+    /// 
+    /// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+    /// </summary>
+    class Problem3LengthOfLongestSubstring
+    {
+        /// <summary>
+        /// Returns the length of the longest substring (without any repeated characters).
+        /// 
+        /// This brute-force solution is slow but it works.
+        /// </summary>
+        public int LengthOfLongestSubstring(string s)
+        {
+            string substring = "";
+            int longestSubstringLength = 0;
+            int j = 0;
+
+            for(int i = 0; i < s.Length; i++)
+            {
+                if (substring.Contains(s[i]))
+                {
+                    substring = "";
+                    j++;
+                    i = j - 1; // the index variable "i" is re-assigned whenever a repeat character is found. This is what causes such a long runtime
+                    continue;
+                }
+                substring += s[i].ToString();
+                longestSubstringLength = substring.Length > longestSubstringLength ? substring.Length : longestSubstringLength; // check if the current substring is the longest so far
+            }
+            return longestSubstringLength;
+        }
+    }
+}
---
+++ Tests/Problem3LengthOfLongestSubstringTest.cs
@@ -0,0 +1,60 @@
+﻿using LeetCode.Solutions.Cs.Problem3LengthOfLongestSubstring;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetCode.Tests
+{
+    public class Problem3LengthOfLongestSubstringTest
+    {
+        Problem3LengthOfLongestSubstring solver = new Problem3LengthOfLongestSubstring();
+
+        /// <summary>
+        /// Normal test case (base case)
+        /// </summary>
+        [Fact]
+        public void BaseCase()
+        {
+            // Arrange & Act
+            string s = "abcabcbb";
+            var answer = solver.LengthOfLongestSubstring(s);
+
+            // Assert
+            Assert.Equal(3, answer);
+        }
+        /// <summary>
+        /// Another normal test case
+        /// </summary>
+        [Fact]
+        public void Test1()
+        {
+            // Arrange & Act
+            string s = "pwwkew";
+            var answer = solver.LengthOfLongestSubstring(s);
+
+            // Assert
+            Assert.Equal(3, answer);
+        }
+        [Fact]
+        public void Test2()
+        {
+            // Arrange & Act
+            string s = "aab";
+            var answer = solver.LengthOfLongestSubstring(s);
+
+            // Assert
+            Assert.Equal(2, answer);
+        }
+        [Fact]
+        public void Test3()
+        {
+            // Arrange & Act
+            string s = "dvdf";
+            var answer = solver.LengthOfLongestSubstring(s);
+
+            // Assert
+            Assert.Equal(3, answer);
+        }
+    }
+}
