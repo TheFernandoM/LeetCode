5caa13b254cfd6c2702a760dff37b7cf778487c1
TheFernandoM <molinaf@sheridancollege.ca>
2022-06-15 01:21:09 -0400

    Added Problem #4 - solution & tests

---
+++ Solutions/C#/Problem4_MedianOfTwoSortedArrays/Problem4MedianOfTwoSortedArrays.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace LeetCode.Solutions.Cs.Problem4MedianOfTwoSortedArrays
+{
+    /// <summary>
+    /// "Given two sorted arrays nums1 and nums2 of size m and n respectively,
+    /// return the median of the two sorted arrays. The overall run time
+    /// complexity should be O(log (m+n))."
+    /// 
+    /// https://leetcode.com/problems/median-of-two-sorted-arrays/
+    /// </summary>
+    public class Problem4MedianOfTwoSortedArrays
+    {
+        /// <summary>
+        /// Brute-force approach, using built-in Array.Sort() method
+        /// </summary>
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            int[] mergedArray = new int[nums1.Length + nums2.Length];
+
+            nums1.CopyTo(mergedArray, 0);
+            nums2.CopyTo(mergedArray, nums1.Length);
+
+            Array.Sort(mergedArray); // is it unethical to use built-in array sort? hah
+                                     // alternative: make my own quicksort implementation
+
+            if(mergedArray.Length % 2 == 0) // if even number of ints in the array
+            {
+                int middleIndex = (mergedArray.Length / 2) - 1; // -1 is because array indices start at 0, but Length counting starts at 1. The difference is 1.
+                double num1 = mergedArray[middleIndex];
+                double num2 = mergedArray[middleIndex + 1];
+                return (num1 + num2) / 2;
+            }
+            if(mergedArray.Length % 2 == 1) // if odd number of ints in the array
+            {
+                int middleIndex = (int) Math.Floor(mergedArray.Length / 2.0d);
+                return mergedArray[middleIndex]; // return the middle index
+            }
+            return -1; // this line is only reached if there is no valid answer
+
+        }
+    }
+}
---
+++ Tests/Problem4MedianOfTwoSortedArrays.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LeetCode.Solutions.Cs.Problem4MedianOfTwoSortedArrays;
+using Xunit;
+
+namespace LeetCode.Tests
+{
+    public class Problem4MedianOfTwoSortedArraysTest
+    {
+        private Problem4MedianOfTwoSortedArrays solver = new Problem4MedianOfTwoSortedArrays();
+
+        /// <summary>
+        /// Normal test case (base case)
+        /// </summary>
+        [Fact]
+        public void BaseCase()
+        {
+            // Arrange & Act
+            int[] input1 = { 1, 3 };
+            int[] input2 = { 2 };
+            var answer = solver.FindMedianSortedArrays(input1, input2);
+
+            // Assert
+            Assert.Equal(2, answer);
+        }
+        [Fact]
+        public void Test1()
+        {
+            // Arrange & Act
+            int[] input1 = { 1, 2 };
+            int[] input2 = { 3, 4 };
+            var answer = solver.FindMedianSortedArrays(input1, input2);
+
+            // Assert
+            Assert.Equal(2.5d, answer);
+        }
+    }
+}
