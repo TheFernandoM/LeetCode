14da18b22fbd5047d7b4ba51cd2c1d9cdbed510f
TheFernandoM <molinaf@sheridancollege.ca>
2022-06-22 22:07:56 -0400

    Add Problem #21 solution and test

---
+++ Solutions/C#/Problem21_MergeTwoSortedLists/Problem21_MergeTwoSortedLists.cs
@@ -0,0 +1,87 @@
+﻿using System;
+namespace LeetCode.Solutions.Cs.Problem21MergeTwoSortedLists
+{
+    /// <summary>
+    /// Problem #21
+    /// "You are given the heads of two sorted linked lists list1 and list2.
+    /// 
+    /// Merge the two lists in a one sorted list.The list should be made by
+    /// splicing together the nodes of the first two lists.
+    ///
+    /// Return the head of the merged linked list."
+    /// 
+    /// https://leetcode.com/problems/merge-two-sorted-lists/
+    /// </summary>
+    public class Problem21MergeTwoSortedLists
+    {
+        /// <summary>
+        /// Runtime complexity: O(2n + n ** 2), which simplifies to O(n**2)
+        /// </summary>
+        public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            if (list1 == null) { return list2; }
+            if (list2 == null) { return list1; }
+
+            int count = 0;
+            ListNode currentNode = list1;
+
+            // find the tail of list1
+            while (currentNode.next != null)
+            {
+                currentNode = currentNode.next;
+                count++;
+            }
+            ListNode tail1 = currentNode;
+
+            // add the head of list 2 to the tail of list 1
+            tail1.next = list2;
+
+            while (currentNode.next != null)
+            {
+                currentNode = currentNode.next;
+                count++;
+            }
+            var comparisonNode = list1.next;
+            // two approaches: one pass, two pass
+
+
+            for (int i = 0; i < count; i++)
+            {
+                currentNode = list1;
+                comparisonNode = list1.next;
+
+                // while it's not the tail
+                while (currentNode.next != null && comparisonNode != null)
+                {
+                    if (currentNode.val > comparisonNode.val)
+                    {
+
+                        // swap their values
+                        int temp = comparisonNode.val;
+                        comparisonNode.val = currentNode.val;
+                        currentNode.val = temp;
+                    }
+                    if (comparisonNode != currentNode)
+                    {
+                        comparisonNode = comparisonNode.next;
+                    }
+                    currentNode = currentNode.next;
+                }
+            }
+            return list1; // answer
+        }
+    }
+    /// <summary>
+    /// Definition for singly-linked list.
+    /// </summary>
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
---
+++ Tests/Problem21_MergeTwoSortedListsTest.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using LeetCode.Solutions.Cs.Problem21MergeTwoSortedLists;
+using LeetCode.Solutions.Cs.Problem27RemoveElement;
+using LeetCode.Solutions.Cs.Problem9PalindromeNumber;
+using Xunit;
+
+namespace LeetCode.Tests
+{
+    public class Problem21MergeTwoSortedListsTest
+    {
+        public Problem21MergeTwoSortedListsTest()
+        {
+
+        }
+        Problem21MergeTwoSortedLists solver = new Problem21MergeTwoSortedLists();
+
+        /// <summary>
+        /// Normal test case (base case)
+        /// </summary>
+        [Fact]
+        public void BaseCaseTest()
+        {
+            // list1 = [1,2,4] 
+            var l1_n3 = new ListNode(4);
+            var l1_n2 = new ListNode(2, l1_n3);
+            var l1_n1 = new ListNode(1, l1_n2);
+
+            // list2 = [1,3,4]
+            var l2_n3 = new ListNode(4);
+            var l2_n2 = new ListNode(3, l2_n3);
+            var l2_n1 = new ListNode(1, l2_n2);
+
+            // Arrange & Act
+            var currentNode = solver.MergeTwoLists(l1_n1, l2_n1);
+
+            // Assert
+            // answer should be: [1,1,2,3,4,4]
+            int[] correctAnswers = new int[] { 1, 1, 2, 3, 4, 4 };
+            foreach(int correctAnswer in correctAnswers)
+            {
+                Assert.Equal(correctAnswer, currentNode.val);
+                if(currentNode.next != null) GotoNextNode(ref currentNode);
+
+            }
+        }
+
+        /// <summary>
+        /// Test case: Both ListNodes are empty
+        ///
+        /// WARNING: According to LeetCode this should return null
+        /// </summary>
+        [Fact]
+        public void BothListsAreEmptyTest()
+        {
+
+            // Arrange & Act
+            ListNode node1 = null;
+            ListNode node2 = null;
+
+            // Assert (answer should be null)
+            ListNode answer = solver.MergeTwoLists(node1, node2);
+            Assert.Null(answer);
+        }
+        /// <summary>
+        /// Test case: Both ListNodes are empty
+        /// </summary>
+        [Fact]
+        public void EmptyListAndZeroListTest()
+        {
+
+            // Arrange & Act
+            ListNode node1 = null;
+            var node2 = new ListNode(0);
+
+            // Assert (answer should be null)
+            var answer = solver.MergeTwoLists(node1, node2);
+            Assert.Equal(0, answer.val);
+            Assert.Null(answer.next);
+        }
+
+        private ListNode GotoNextNode(ref ListNode currentNode)
+        {
+            currentNode = currentNode.next;
+            return currentNode.next;
+        }
+    }
+}
