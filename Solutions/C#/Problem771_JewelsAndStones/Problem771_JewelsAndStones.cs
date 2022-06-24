77c8f6bf7d7cf703b6747e896063546ecd4debc9
TheFernandoM <molinaf@sheridancollege.ca>
2022-06-24 18:48:16 -0400

    Add Problem #771 solution and test

---
+++ Solutions/C#/Problem771_JewelsAndStones/Problem771_JewelsAndStones.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace LeetCode.Solutions.Cs.Problem771JewelsAndStones
+{
+    /// <summary>
+    /// Problem # 771
+    /// "You're given strings jewels representing the types of stones that are
+    /// jewels, and stones representing the stones you have. Each character in
+    /// stones is a type of stone you have. You want to know how many of the
+    /// stones you have are also jewels.
+    /// 
+    /// Letters are case sensitive, so "a" is considered a different type of
+    /// stone from "A"."
+    // 
+    /// https://leetcode.com/problems/jewels-and-stones/
+    /// </summary>
+    public class Problem771JewelsAndStones
+    {
+        /// <summary>
+        /// Runtime complexity: O(n**2)
+        /// </summary>
+        public int NumJewelsInStones(string jewels, string stones)
+        {
+
+            int result = 0;
+            foreach (char jewel in jewels)
+            {
+                foreach (char stone in stones)
+                {
+                    if (jewel == stone) { result++; }
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Tests/Problem771_JewelsAndStonesTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using LeetCode.Solutions.Cs.Problem771JewelsAndStones;
+using LeetCode.Solutions.Cs.Problem9PalindromeNumber;
+using Xunit;
+
+namespace LeetCode.Tests
+{
+    public class Problem771JewelsAndStonesTest
+    {
+        public Problem771JewelsAndStonesTest()
+        {
+
+        }
+        Problem771JewelsAndStones solver = new Problem771JewelsAndStones();
+
+        /// <summary>
+        /// Normal test case (base case)
+        /// </summary>
+        [Fact]
+        public void BaseCaseTest()
+        {
+            // Arrange & Act
+            string jewels = "aA";
+            string stones = "aAAbbbb";
+
+            var answer = solver.NumJewelsInStones(jewels, stones);
+
+            // Assert
+            Assert.Equal(3, answer);
+        }
+        /// <summary>
+        /// Normal test case (base case)
+        /// </summary>
+        [Fact]
+        public void BaseCaseTest2()
+        {
+            // Arrange & Act
+            string jewels = "z";
+            string stones = "ZZ";
+
+            var answer = solver.NumJewelsInStones(jewels, stones);
+
+            // Assert
+            Assert.Equal(0, answer);
+        }
+    }
+}
