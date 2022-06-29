16ef2703193840ec2b61f2be911e3a318447c963
TheFernandoM <molinaf@sheridancollege.ca>
2022-06-29 14:16:59 -0400

    Create Problem #35 Solution+Test (Work in progress)

---
+++ Solutions/C#/Problem35_SearchInsertPosition/Problem35_SearchInsertPosition.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Solutions.Cs.Problem3LengthOfLongestSubstring
+{
+    /// <summary>
+    /// #35: Search Insert Position
+    ///
+    /// Given a sorted array of distinct integers and a target value, return the index if the target is 
+    /// found. If not, return the index where it would be if it were inserted in order.
+    ///
+    /// You must write an algorithm with O(log n) runtime complexity.
+    /// 
+    /// https://leetcode.com/problems/search-insert-position/
+    /// </summary>
+    class Problem35SearchInsertPosition
+    {
+        /// <summary>
+        /// Returns the length of the longest substring (without any repeated characters).
+        /// 
+        /// This brute-force solution is slow but it works.
+        /// </summary>
+        public int SearchInsert(int[] nums, int target)
+        {
+            int answer = 0;
+
+            return 0;
+        }
+    }
+}
---
+++ Tests/Poblem35_SearchInsertPositionTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace LeetCode.Tests
+{
+    public class Poblem35_SearchInsertPositionTest
+    {
+        public Poblem35_SearchInsertPositionTest()
+        {
+        }
+    }
+}
